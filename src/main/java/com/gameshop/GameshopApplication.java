a28f8718d826727a6e6facbd7d508409645d6a3b
Disuqi <disuqi.hijazi.7@gmail.com>
2021-10-02 22:45:48 +0100

    added Game entity

--- src/main/java/com/gameshop/GameshopApplication.java
+++ src/main/java/com/gameshop/GameshopApplication.java
@@ -1,4 +1,4 @@
-package gameshop.gameshop;
+package com.gameshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/gameshop/entities/Game.java
@@ -0,0 +1,24 @@
+package com.gameshop.entities;
+import javax.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Game{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column
+    private float price;
+
+    public Game(String title, float price){
+        this.title = title;
+        this.price = price;
+    }
+}
