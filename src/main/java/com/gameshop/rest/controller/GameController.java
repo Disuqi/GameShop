022e466805edb0d3027a4c86d2e9afc5883130be
Disuqi <disuqi.hijazi.7@gmail.com>
2021-10-03 22:42:20 +0100

    added GameDTO

--- src/main/java/com/gameshop/rest/controller/GameController.java
+++ src/main/java/com/gameshop/rest/controller/GameController.java
@@ -1,5 +1,6 @@
 package com.gameshop.rest.controller;
 import com.gameshop.persistence.domain.Game;
+import com.gameshop.rest.dto.GameDTO;
 import com.gameshop.service.GameService;
 
 import java.util.List;
@@ -19,42 +20,42 @@ public class GameController {
     private GameService service;
 
     @PostMapping("/add")
-    public Game addGame(@RequestBody Game game){
+    public GameDTO addGame(@RequestBody Game game){
         return this.service.addGame(game);
     }
 
     @GetMapping("/get/{id}")
-    public Game getGame(@PathVariable long id){
+    public GameDTO getGame(@PathVariable long id){
         return this.service.getGame(id);
     }
 
     @GetMapping("/getByTitle/{title}")
-    public Game getGameByTitle(@PathVariable String title){
+    public GameDTO getGameByTitle(@PathVariable String title){
         return this.service.getGameByTitle(title);
     }
 
     @GetMapping("/getByPrice/{price}")
-    public List<Game> getGamesByPrice(@PathVariable float price){
+    public List<GameDTO> getGamesByPrice(@PathVariable float price){
         return this.service.getGamesByPrice(price);
     }
 
     @GetMapping("/getByPriceGreaterThan/{price}")
-    public List<Game> getGamesPriceHigherThan(@PathVariable float price){
+    public List<GameDTO> getGamesPriceHigherThan(@PathVariable float price){
         return this.service.getGamesByPriceGreaterThan(price);
     }
 
     @GetMapping("/getByPriceLessThan/{price}")
-    public List<Game> getGamesLowerThan(@PathVariable float price){
+    public List<GameDTO> getGamesLowerThan(@PathVariable float price){
         return this.service.getGamesByPriceLessThan(price);
     }
 
     @GetMapping("/getAll")
-    public List<Game> getAllGames(){
+    public List<GameDTO> getAllGames(){
         return this.service.getAllGames();
     }
 
     @PutMapping("/update/{id}")
-    public Game updateGame(@PathVariable long id, @RequestBody Game game){
+    public GameDTO updateGame(@PathVariable long id, @RequestBody Game game){
         return this.service.updateGame(id, game);
     }
 
---
+++ src/main/java/com/gameshop/rest/dto/GameDTO.java
@@ -0,0 +1,14 @@
+package com.gameshop.rest.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GameDTO {
+    private long id;
+
+    private String title;
+
+    private float price;
+}
