5a83ba2510f3fee8199992c5af2ba96cbea1e2c4
Disuqi <disuqi.hijazi.7@gmail.com>
2021-10-03 15:54:49 +0100

    added repo

---
+++ src/main/java/com/gameshop/repositories/GameRepo.java
@@ -0,0 +1,11 @@
+package com.gameshop.repositories;
+
+import com.gameshop.entities.Game;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepo extends JpaRepository<Game, Long>{
+    
+}
