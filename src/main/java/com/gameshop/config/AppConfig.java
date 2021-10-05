3815639f87dda187802f155fb69627b5b2c5dec8
Disuqi <disuqi.hijazi.7@gmail.com>
2021-10-05 17:38:48 +0100

    added gameshop.jar version 0.0.1

--- src/main/java/com/gameshop/config/AppConfig.java
+++ src/main/java/com/gameshop/config/AppConfig.java
@@ -3,7 +3,6 @@ package com.gameshop.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class AppConfig {
